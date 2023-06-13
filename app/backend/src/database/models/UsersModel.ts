16902a50d584a7ff565de9d237cba41970efdc40
Pedro Viana <pedroviana.developer13@gmail.com>
2023-06-13 19:06:31 -0300

    feat: User model, migration and seeder

---
+++ app/backend/src/database/models/UsersModel.ts
@@ -0,0 +1,42 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare readonly id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+  modelName: 'users',
+});
+
+export default User;
